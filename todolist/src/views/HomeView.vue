89efcbb5521418432dc19fea0bbfa1f1a9c0fa02
chuck47286 <chuck472861399@163.com>
2024-02-06 15:34:35 +0800

    [feat] reactive + torefs 定义对象

--- todolist/src/views/HomeView.vue
+++ todolist/src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <template>
   <!-- 编写html内容 -->
   <!-- 1.子组件引入 -->
-  <nav-header></nav-header>
+  <!-- <nav-header></nav-header>
   <nav-main></nav-main>
-  <nav-footer></nav-footer>
-  <!-- 2. ref定义数据 -->
+  <nav-footer></nav-footer> -->
+
+  <!-- 2. ref定义数据 or reactive + torefs -->
   <div>
     {{ num }}
   </div>
@@ -17,6 +18,20 @@
   <div>
     {{ obj }}
   </div>
+
+  <!-- 3. reactive 定义对象 -->
+  <!-- <div>
+    {{ data.num }}
+  </div>
+  <div>
+    {{ data.name }}
+  </div>
+  <div>
+    {{ data.arr.slice(0,2) }}
+  </div>
+  <div>
+    {{ data.obj }}
+  </div> -->
 </template>
 
 
@@ -25,7 +40,7 @@
 import NavHeader from '@/components/navHeader/NavHeader.vue'
 import NavMain from '@/components/navMain/NavMain.vue'
 import NavFooter from '@/components/navFooter/NavFooter.vue'
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, reactive, toRefs} from 'vue'
 export default defineComponent({
   // 当前组件名称
   name: 'Home',
@@ -40,17 +55,29 @@ export default defineComponent({
     NavFooter
   },
   setup(props, ctx) {
-    let num = ref(10)
-    let name = ref('jack')
-    let arr = ref(['a', 'b', 'c', 'd'])
-    let obj = ref({
-      age: 20
+    // let num = ref(10)
+    // let name = ref('jack')
+    // let arr = ref(['a', 'b', 'c', 'd'])
+    // let obj = ref({
+    //   age: 20
+    // })
+    let data = reactive({
+      num: 10,
+      name: 'jack',
+      age: 20,
+      obj: {
+        price: 20
+      },
+      arr: ['a', 'b', 'c', 'd']
     })
     return {
-      num,
-      name,
-      arr,
-      obj
+      // num,
+      // name,
+      // arr,
+      // obj
+
+      // data
+      ...toRefs(data)
     }
   }
 })
