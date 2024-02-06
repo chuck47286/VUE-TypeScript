6bbfcc5a73acd8b4284641e8dee8c970d4df5fa6
chuck47286 <chuck472861399@163.com>
2024-02-06 15:21:12 +0800

    [feat] vue 子组件映入/ref引入数据

---
+++ todolist/src/components/navFooter/NavFooter.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- 编写html内容 -->
+    <div>
+        fotter
+    </div>
+</template>
+  
+  
+<script>
+// 编写js内容
+import { defineComponent } from 'vue'
+export default defineComponent({
+    // 当前组件名称
+    name: 'Home',
+    // 接收父组件的数据
+    props: {
+
+    },
+    // 定义子组件
+    components: {
+
+    },
+    setup(props, ctx) {
+        return {
+
+        }
+    }
+})
+</script>
+  
+<style scoped lang="scss">
+/* CSS样式+作用域(当前组件生效) */
+</style>
--- todolist/src/views/HomeView.vue
+++ todolist/src/views/HomeView.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- 编写html内容 -->
+  <!-- 1.子组件引入 -->
+  <nav-header></nav-header>
+  <nav-main></nav-main>
+  <nav-footer></nav-footer>
+  <!-- 2. ref定义数据 -->
+  <div>
+    {{ num }}
+  </div>
+  <div>
+    {{ name }}
+  </div>
+  <div>
+    {{ arr.slice(0,2) }}
+  </div>
+  <div>
+    {{ obj }}
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
+<script>
+// 编写js内容
+import NavHeader from '@/components/navHeader/NavHeader.vue'
+import NavMain from '@/components/navMain/NavMain.vue'
+import NavFooter from '@/components/navFooter/NavFooter.vue'
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  // 当前组件名称
+  name: 'Home',
+  // 接收父组件的数据
+  props: {
+
+  },
+  // 定义子组件
   components: {
-    HelloWorld
+    NavHeader,
+    NavMain,
+    NavFooter
+  },
+  setup(props, ctx) {
+    let num = ref(10)
+    let name = ref('jack')
+    let arr = ref(['a', 'b', 'c', 'd'])
+    let obj = ref({
+      age: 20
+    })
+    return {
+      num,
+      name,
+      arr,
+      obj
+    }
   }
-}
+})
 </script>
+
+<style scoped lang="scss">
+/* CSS样式+作用域(当前组件生效) */
+</style>
